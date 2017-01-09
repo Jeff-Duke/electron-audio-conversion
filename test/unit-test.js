786dc6f15d4fe1a222c81faef1b89826d1103dc8
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-09 10:39:00 -0700

    Add base functionality for test suite

---
+++ test/unit-test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+
+describe('test bundle', () => {
+  context('find server', () => {
+    it('should work', () => {
+      assert.equal();
+    });
+  });
+});
