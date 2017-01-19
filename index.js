f4be7c072d4a1d6ca35ca905c4f4195309cb5607
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-19 16:21:18 -0700

    Add working test for getSongDuration

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const getSongDuration = (filePath) => {
 };
 
 const getSongTags = (track) => {
-  const {filePath} = track;
+  const  { filePath }  = track;
   const tagsPromise = new Promise((resolve, reject) => {
     id3({
       file: filePath,
@@ -59,6 +59,7 @@ const createSongUri = (filePath, mimetype) => {
 
 module.exports = {
   getSongDuration,
+  getSongTags,
   createSongUri,
   createSongObject
 };
--- test/unit-test.js
+++ test/unit-test.js
@@ -7,16 +7,15 @@ const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
 
-describe('test bundle', function() {
-  it('should work', function() {
+describe('test bundle', () => {
+  it('should work', () => {
     assert.equal();
   });
 });
 
-describe('file conversion process', function() {
-  it('should get the song\'s duration', function(done) {
-   assert.eventually.equal(Promise.resolve(h5.getSongDuration('/Users/bcg/Desktop/Tracks/Salute.mp3')), 168.098, "This had better be true, eventually").notify(done);
+describe('file conversion process', () => {
+  it('should get the song\'s duration', (done) => {
+     assert.eventually.equal(Promise.resolve(h5.getSongDuration('/Users/bcg/Desktop/Tracks/Salute.mp3')), 168.098, "This should return time in seconds").notify(done);
   });
-
 
 });
