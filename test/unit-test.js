a8f7b25d0cd76ae1d2328d4fdf76b2348c413436
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-19 22:14:53 -0700

    Refactor filePath

--- test/unit-test.js
+++ test/unit-test.js
@@ -8,34 +8,36 @@ const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
 
+const filePath = '/Users/bcg/Desktop/Tracks/Salute.mp3'
+
 describe('file conversion process', () => {
 
   it('should get the song\'s duration', (done) => {
-    return assert.eventually.equal(Promise.resolve(h5.getSongDuration('/Users/bcg/Desktop/Tracks/Salute.mp3')), 168.098, "This should return time in seconds").notify(done);
+    return assert.eventually.equal(Promise.resolve(h5.getSongDuration(filePath)), 168.098, "This should return time in seconds").notify(done);
   });
 
   it('should return a song\'s tags', (done) => {
     //does not require duration, but will have it in our actual function because
     //of how we are chainging promises
-    const track = { filePath: '/Users/bcg/Desktop/Tracks/Salute.mp3' }
+    const track = { filePath: filePath }
 
     expect(Promise.resolve(h5.getSongTags(track))).to.eventually.have.property('artist').notify(done)
   });
 
   it('should return a song object', (done) => {
-     expect(Promise.resolve(h5.createSongObject('/Users/bcg/Desktop/Tracks/Salute.mp3'))).to.eventually.have.property('duration', 168.098)
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('duration', 168.098)
 
-     expect(Promise.resolve(h5.createSongObject('/Users/bcg/Desktop/Tracks/Salute.mp3'))).to.eventually.have.property('artist', 'Future')
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('artist', 'Future')
 
-     expect(Promise.resolve(h5.createSongObject('/Users/bcg/Desktop/Tracks/Salute.mp3'))).to.eventually.have.property('album', 'Purple Reign')
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('album', 'Purple Reign')
 
-     expect(Promise.resolve(h5.createSongObject('/Users/bcg/Desktop/Tracks/Salute.mp3'))).to.eventually.have.property('title', 'Salute\u0000').notify(done)
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('title', 'Salute\u0000').notify(done)
   });
 
   it('should create a song url by converting to a dataurl', (done) => {
-    const TEST_FILE = fs.readFileSync('/Users/bcg/Desktop/Tracks/Salute.mp3');
+    const TEST_FILE = fs.readFileSync(filePath);
     const TEST_DATAURL = `data:audio/mp3;base64,${TEST_FILE.toString('base64')}`;
 
-    assert.eventually.equal(Promise.resolve(h5.createSongUri('/Users/bcg/Desktop/Tracks/Salute.mp3', 'audio/mp3')), TEST_DATAURL, "This should return the same dataurl as or TEST_DATAURL variable").notify(done);
+    assert.eventually.equal(Promise.resolve(h5.createSongUri(filePath, 'audio/mp3')), TEST_DATAURL, "This should return the same dataurl as or TEST_DATAURL variable").notify(done);
   })
 });
