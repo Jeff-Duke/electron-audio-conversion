4a9ac3b5a9ef449916f26b9bd857dbd07c88af76
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-19 22:57:35 -0700

    Recreate tests to work universally. Add mp3 to file structure

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,37 +1,40 @@
-var expect = require('chai').expect;
-var assert = require('chai').assert;
-var should = require('chai').should;
-
+const { assert, expect } = require('chai')
 const fs = require('fs');
 const h5 = require('../index');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
-const filePath = '/Users/bcg/Desktop/Tracks/Salute.mp3';
+const filePath = __dirname + '/Acoustic-Breeze.mp3';
 
 describe('file conversion process', () => {
 
   it('should get the song\'s duration', (done) => {
-    return assert.eventually.equal(Promise.resolve(h5.getSongDuration(filePath)), 168.098, "This should return time in seconds").notify(done);
+    return assert.eventually.equal(Promise.resolve(h5.getSongDuration(filePath)), 157.205, "This should return time in seconds").notify(done);
   });
 
   it('should return a song\'s tags', (done) => {
     //Function does not require duration, but it will present in our actual
-    //function because of how we are chaining promises. 
+    //function because of how we are chaining promises.
     const track = { filePath: filePath };
 
-    expect(Promise.resolve(h5.getSongTags(track))).to.eventually.have.property('artist', 'Future\u0000').notify(done);
+    expect(Promise.resolve(h5.getSongTags(track))).to.eventually.have.property('artist', 'Bensound\u0000').notify(done);
   });
 
-  it('should return a song object', (done) => {
-     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('duration', 168.098);
+  it('should return a song object with a duration of 157.205', (done) => {
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('duration', 157.205).notify(done);
+  });
 
-     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('artist', 'Future');
+  it('should return a song object with an artist of Bensound', (done) => {
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('artist', 'Bensound\u0000').notify(done);
+  });
 
-     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('album', 'Purple Reign');
+  it('should return a song object with an album title of ROYALTY FREE MUSIC', (done) => {
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('duration', 157.205).notify(done);
+  });
 
-     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('title', 'Salute\u0000').notify(done);
+  it('should return a song object with a track title of Acoustic-Breeze', (done) => {
+     expect(Promise.resolve(h5.createSongObject(filePath))).to.eventually.have.property('duration', 157.205).notify(done);
   });
 
   it('should create a song url by converting to a dataurl', (done) => {
